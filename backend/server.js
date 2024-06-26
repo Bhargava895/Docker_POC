c580f7875fdc7d581e3b2160b40872aaaefc51a1
Bhargava895 <37539869+Bhargava895@users.noreply.github.com>
2024-06-26 19:03:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Cybage (Backend!)');
+  res.send('Hello Code backend (Backend!)');
 });
 
 app.listen(port, () => {
